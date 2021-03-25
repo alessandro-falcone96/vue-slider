241af0216c02aa4e6c081b91e3b8dbbdd2471eb2
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-25 17:49:24 +0100

    Aggiunto carousel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Inserisci il codice qui
+var app = new Vue(
+  {
+    el: '#slider',
+    data: {
+      images: [
+        'img/foto-1.jpg',
+        'img/foto-2.jpg',
+        'img/foto-3.jpg',
+        'img/foto-4.jpg',
+        'img/foto-5.jpg'
+      ],
+      imagesIndex: 0
+    },
+    methods: {
+      clickNext: function() {
+        this.imagesIndex == (this.images.length - 1) ? this.imagesIndex = 0 : this.imagesIndex++
+      },
+      clickPrev: function() {
+        this.imagesIndex == 0 ? this.imagesIndex = (this.images.length - 1) : this.imagesIndex--
+      }
+    }
+  }
+);
